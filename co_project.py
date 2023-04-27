c7b0063b59895d153dd8b9764233f9aa2f3c808b
Shubhamgoe <129890402+Shubhamgoe@users.noreply.github.com>
2023-04-27 15:47:02 +0530

    Update co_project.py

--- co_project.py
+++ co_project.py
@@ -1,1 +1,9 @@
-
+def Decimel_to_binary(num):             // code to convert decimel to binary
+    bin_num = ""
+    if num == 0:
+        bin_num = "0"
+    else:
+        while num > 0:
+            bin_num = str( num % 2 ) + bin_num
+            num = num // 2
+    return bin_num
