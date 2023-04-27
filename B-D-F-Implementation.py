09f78b4d1c1d7d619d64a847eb1ab19617ae4ce7
Vedika Agarwal <131861772+27vedika@users.noreply.github.com>
2023-04-27 19:44:37 +0530

    Update B-D-F-Implementation.py

--- B-D-F-Implementation.py
+++ B-D-F-Implementation.py
@@ -9,44 +9,44 @@ def binary(x,n):
 def movImm(lst): #00010 mov
     r=binary(int(lst[1][-1]),3)
     val=binary(int(lst[2][1:]),7)
-    print ("00010","0",r,val)
+    print ("00010"+"0"+r+val)
 def rightShift(lst): #01000 rs
     r=binary(int(lst[1][-1]),3)
     val=binary(int(lst[2][1:]),7)
-    print ("01000","0",r,val)
+    print ("01000"+"0"+r+val)
 def leftShift(lst): #01001 ls
     r=binary(int(lst[1][-1]),3)
     val=binary(int(lst[2][1:]),7)
-    print ("01001","0",r,val)
+    print ("01001"+"0"+r+val)
 
 def movReg(lst): #00011 mov
     reg1=binary(int(lst[1][-1]),3)
     reg2=binary(int(lst[2][-1]),3)
-    print ("00011", "00000", reg1, reg2)
+    print ("00011"+"00000"+reg1+reg2)
 def divide(lst): #00111 div
     reg1=binary(int(lst[1][-1]),3)
     reg2=binary(int(lst[2][-1]),3)
-    print ("00111", "00000", reg1, reg2)
+    print ("00111"+"00000"+reg1+reg2)
 def invert(lst): #01101 not
     reg1=binary(int(lst[1][-1]),3)
     reg2=binary(int(lst[2][-1]),3)
-    print ("01101", "00000", reg1, reg2)
+    print ("01101"+"00000"+reg1+reg2)
 def compare(lst): #01110 cmp
     reg1=binary(int(lst[1][-1]),3)
     reg2=binary(int(lst[2][-1]),3)
-    print ("01110", "00000", reg1, reg2)
+    print ("01110"+"00000"+reg1+reg2)
 
 def load(lst, addr): #00100 ld
     r=binary(int(lst[1][-1]),3)
     addr=binary(addr, 7)
-    print ("00100", "0", r,addr)
+    print ("00100"+"0"+r+addr)
 def store(lst, addr): #00101 st
     r=binary(int(lst[1][-1]),3)
     addr=binary(addr,7)
-    print ("00101", "0", r, addr)
+    print ("00101"+"0"+r+addr)
 
 def halt(): #11010 hlt
-    print ("11010", "00000000000")
+    print ("11010"+"00000000000")
 
 code=[["mov", "R1", "$12"],
       ["ld", "R1", "var_name1"],
