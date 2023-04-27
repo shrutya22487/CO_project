92a3a86e996c3beff0a608b41cfb0aac6a6ea914
Vedika Agarwal <131861772+27vedika@users.noreply.github.com>
2023-04-27 19:22:37 +0530

    Update B-D-F-Implementation.py

--- B-D-F-Implementation.py
+++ B-D-F-Implementation.py
@@ -5,26 +5,41 @@ def binary(x,n):
         x=x//2
     return "".join(lst)
 
+
 def movImm(lst): #00010 mov
     r=binary(int(lst[1][-1]),3)
     val=binary(int(lst[2][1:]),7)
     print ("00010","0",r,val)
-
 def rightShift(lst): #01000 rs
     r=binary(int(lst[1][-1]),3)
     val=binary(int(lst[2][1:]),7)
     print ("01000","0",r,val)
-
 def leftShift(lst): #01001 ls
     r=binary(int(lst[1][-1]),3)
     val=binary(int(lst[2][1:]),7)
     print ("01001","0",r,val)
 
+def movReg(lst): #00011 mov
+    reg1=binary(int(lst[1][-1]),3)
+    reg2=binary(int(lst[2][-1]),3)
+    print ("00011", "00000", reg1, reg2)
+def divide(lst): #00111 div
+    reg1=binary(int(lst[1][-1]),3)
+    reg2=binary(int(lst[2][-1]),3)
+    print ("00111", "00000", reg1, reg2)
+def invert(lst): #01101 not
+    reg1=binary(int(lst[1][-1]),3)
+    reg2=binary(int(lst[2][-1]),3)
+    print ("01101", "00000", reg1, reg2)
+def compare(lst): #01110 cmp
+    reg1=binary(int(lst[1][-1]),3)
+    reg2=binary(int(lst[2][-1]),3)
+    print ("01110", "00000", reg1, reg2)
+
 def load(lst, addr): #00100 ld
     r=binary(int(lst[1][-1]),3)
     addr=binary(addr, 7)
     print ("00100", "0", r,addr)
-
 def store(lst, addr): #00101 st
     r=binary(int(lst[1][-1]),3)
     addr=binary(addr,7)
@@ -36,15 +51,22 @@ def halt(): #11010 hlt
 code=[["mov", "R1", "$12"],
       ["ld", "R1", "var_name1"],
       ["st", "R2", "var_name2"],
+      ["not", "R1", "R2"],
+      ["mov", "R1", "R2"],
+      ["cmp", "R1", "R2"],
       ["rs", "R1", "$12"],
       ["ls", "R1", "$13"],
+      ["div", "R3", "R4"],
       ["hlt"]]
-addr=len(code)-1
 
+addr=len(code)-1
 for instr in code:
     op=instr[0]
-    if op=="mov" and ("$" in instr[2]):
-        movImm(instr)
+    if op=="mov":
+        if ("$" in instr[2]):
+            movImm(instr)
+        else:
+            movReg(instr)
     elif op=="rs":
         rightShift(instr)
     elif op=="ls":
@@ -55,5 +77,11 @@ for instr in code:
     elif op=="st":
         addr+=1
         store(instr, addr)
+    elif op=="div":
+        divide(instr)
+    elif op=="not":
+        invert(instr)
+    elif op=="cmp":
+        compare(instr)
     elif op=="hlt":
         halt()
