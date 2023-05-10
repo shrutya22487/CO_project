5e321aee26014b2fe254e2cde9eb96bd1d180265
Sneha Agarwal <119069324+sneha-iiitd@users.noreply.github.com>
2023-05-10 11:10:35 +0530

    Update SC_CO.py

--- SC_CO.py
+++ SC_CO.py
@@ -1,44 +1,51 @@
 def check_instruction_A(instruction_list): # checks if instruction is of type A then returns 0
-    if (len(instruction_list)==4) and (instruction_list[0] in command_A) and (instruction_list[1] in registers) and (instruction_list[2] in registers) and (instruction_list[3] in registers):
-        return 0
+    if len(instruction_list)==4 :
+        if (instruction_list[0] in command_A) and (instruction_list[1] in registers) and (instruction_list[2] in registers) and (instruction_list[3] in registers):
+            return 0
     return 1
 def check_instruction_B(instruction_list): # checks if instruction is of type B then returns 0
     try:
         int(instruction_list[2][1:])
     except:
         return 1
-    if (len(instruction_list)==3) and (instruction_list[0] in command_B) and (instruction_list[1] in registers) and (0<=int(instruction_list[2][1:])<=127):
-        return 0
+    if len(instruction_list)==3 :
+        if (instruction_list[0] in command_B) and (instruction_list[1] in registers) and (0<=int(instruction_list[2][1:])<=127):
+            return 0
     return 1
 def check_instruction_C(instruction_list): # checks if instruction is of type C then returns 0
-    if (len(instruction_list)==3) and (instruction_list[0] in command_C) and (instruction_list[1] in registers) and (instruction_list[2] in registers):
-        return 0
+    if len(instruction_list)==3:
+        if  (instruction_list[0] in command_C) and (instruction_list[1] in registers) and (instruction_list[2] in registers):
+            return 0
     return 1
 def check_instruction_D(instruction_list): # checks if instruction is of type D then returns 0
-    if (len(instruction_list)==3) and (instruction_list[0] in command_D) and (instruction_list[1] in registers) and type(instruction_list[2]) == 'str':
-        return 0
+    if len(instruction_list)==3:
+        if (instruction_list[0] in command_D) and (instruction_list[1] in registers) and type(instruction_list[2]) == 'str' :
+            return 0
     return 1
 def check_instruction_E(instruction_list): # checks if instruction is of type E then returns 0
-    if (len(instruction_list)==2) and (instruction_list[0] in command_E) and type(instruction_list[1]) == 'str':
-        return 0
+    if len(instruction_list)==2:
+        if (instruction_list[0] in command_E) and type(instruction_list[1]) == 'str':
+            return 0
     return 1
 def check_instruction_F(instruction_list): # checks if instruction is of type F then returns 0
     if instruction_list==['hlt']:
         return 0
     return 1
 def check_label(instruction_list): # checks if instruction is of type label the returns 0
-                                   # i am assuming that the instructions are under the labels not in immediate continuation
-    if instruction_list[0][-1] == ':' and len(instruction_list) == 1:
-        return 0
+    if len(instruction_list) == 1:                               # i am assuming that the instructions are under the labels not in immediate continuation
+        if instruction_list[0][-1] == ':' :
+            return 0
     return 1
 
 def check_variables(instruction_list):
-    if instruction_list[0] == 'var' and len( instruction_list ) == 2 and type( instruction_list[1] == 'str'):
-        return 0
+    #print( " instruction_list is :" ,instruction_list )
+    if len( instruction_list ) == 2:
+        if instruction_list[0] == 'var' and type( instruction_list[1] == 'str'):
+            return 0
     return 1
 
 def check_flag( instruction_list ):
-    if ['FLAGS'] in instruction_list and instruction_list[0] != 'mov':
+    if 'FLAGS' in instruction_list and instruction_list[0] != 'mov':
         print(" invalid use of FLAGS register")
         return 0
     return 1
@@ -46,7 +53,7 @@ def check_flag( instruction_list ):
 ########## main ############
 
 input=open("/home/mc/a.txt",'r')
-command_list=input.readlines()
+command_list=f.readlines()
 f=open("/home/mc/output_file.txt",'w')
 for i in range(len(command_list)):
     command_list[i]=command_list[i].split()
@@ -69,12 +76,13 @@ label = []
 lst, ind = [] , 0
 flag = 0
 for i in range(l):
-    if not check_variables( command_list[i] ):         # assuming that after variable declaration length of instruction would not be 2
-        lst.append(command_list[i][1])    # making variable list
-        ind += 1
-    elif command_list[i] == []:
+    if command_list[i] == []:         # assuming that after variable declaration length of instruction would not be 2
         ind += 1
         continue
+    elif not check_variables( command_list[i] ):
+        #print( command_list[i] )
+        lst.append(command_list[i][1])    # making variable list
+        ind += 1
     else:
         break
 
@@ -88,65 +96,69 @@ for i in range(l): #starting to check for conditions
         ######checks for condition a ########
 
         if check_variables( command_list[i] ) and check_label( command_list[i] ) and check_instruction_A(command_list[i]) and  check_instruction_B(command_list[i]) and check_instruction_C(command_list[i]) and check_instruction_D(command_list[i]) and check_instruction_E(command_list[i]) and check_instruction_F(command_list[i]):
-            s = f"{i+1} incorrect instruction name or register name"
-            print(i+1, "incorrect instruction name or register name")
             flag += 1
+            s = f"{i+1} incorrect instruction name or register name"
+            #print(i+1, "incorrect instruction name or register name")
             f.write(s)
-            continue
+            continue   # change f as output in file
 
         ######checks for condition b #########
 
         if command_list[i][0] in {"ld","st"}:
             if command_list[i][2] not in lst:
-                print("use of undefined variables")
                 flag += 1
+                s = f"{i+1} use of undefined variables"
+                #print("use of undefined variables")
+                f.write(s)
                 continue
 
         ###### checks for condition G #######
 
         if i>= ind and not check_variables( command_list[i] ):
-            print("variables not defined at beginning")
             flag += 1
+            s = f"{i+1} variables not defined at beginning "
+            #print(i + 1 , "variables not defined at beginning")
+            f.write(s)
             continue
 
         ######checks for condition e ########
 
         if not check_instruction_B(command_list[i]) and not(0<=int(command_list[i][2][1:])<=127):
-            s=f"{i+1} illegal immediate values"
-            print(i+1, "illegal immediate values")
             flag += 1
+            s=f"{i+1} illegal immediate values"
+            #print(i+1, "illegal immediate values")
             f.write(s)
             continue
 
         ######checks for condition c ########
         if not check_instruction_E( command_list[i] ) and command_list[i][1] not in label:
-            print(command_list[i][1])
-            s=f"{i+1} use of undefined labels"
-            print(i+1, " use of undefined labels")
             flag += 1
+            #print(command_list[i][1])
+            s=f"{i+1} use of undefined labels"
+            #print(i+1, " use of undefined labels")
             f.write(s)
             continue
 
         ###### checks for condition F ########
         if not check_instruction_E( command_list[i] ) and  ( command_list[i][1][0 : len( command_list[i] )-1 ] in lst ):
-            s=f"{i+1} misuse variable as label "
-            print(i+1," misuse variable as label")
             flag += 1
+            s=f"{i+1} misuse variable as label "
+            #print(i+1," misuse variable as label")
             f.write(s)
             continue
 
 
         if not check_instruction_D( command_list[i]) and ( command_list[i][2] not in lst ):
-            s=f"{i+1} misuse label as variable "
-            print(i+1," misuse label as variable")
             flag += 1
+            s=f"{i+1} misuse label as variable "
+            #print(i+1," misuse label as variable")
             f.write(s)
             continue
 
 
         ##### checks for condition D#######
 
-        if not check_flag( command_list [i] ):
+        if 'FLAGS' in instruction_list and instruction_list[0] != 'mov':
             flag += 1
             continue
 
@@ -154,18 +166,18 @@ for i in range(l): #starting to check for conditions
 ######checks for condition H ########
 
 if ['hlt'] not in command_list:
-    s=f'{l} hlt is missing'
-    print(l,"hlt is missing")
-    f.write(s)
     flag += 1
+    s=f'{l} hlt is missing'
+    #print(l,"hlt is missing")
+    f.write(s)
 
 ######checks for condition I ########
 
-if ['hlt'] in command_list and ['hlt']!=command_list[l-1]:
-    s=f'{l} hlt is not last instruction'
-    print(l," hlt is not last instruction")
-    f.write(s)
+if ['hlt'] in command_list and ['hlt'] != command_list[-1]:
     flag += 1
-    
+    s=f'{l} hlt is not last instruction'
+    #print(l," hlt is not last instruction")
+    f.write(s)
+
+input.close()
 f.close()
-output.close()
