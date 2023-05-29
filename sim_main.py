e9678da346ee32b43b96e4225f8d96b0b11c0c13
Shubham Goel <129890402+Shubhamgoe@users.noreply.github.com>
2023-05-29 12:46:53 +0530

    Update sim_main.py

--- sim_main.py
+++ sim_main.py
@@ -106,6 +106,26 @@ def st(instr):
     FLAGS="0000000000000000"
     return i+1
 
+def jmp(instr):   # "01111"
+    mem_addr = instr[10:]
+    return decimal(mem_addr)
+  
+def jlt(instr):  # "11100"
+    if FLAGS[14] == "1":
+        mem_addr = instr[10:]
+        return decimal(mem_addr)
+      
+def jgt(instr):  # "11101"
+    if FLAGS[15] == "1":
+        mem_addr = instr[10:]
+        return decimal(mem_addr)
+  
+def je(instr):  # "11111"
+    if FLAGS[16] == "1":
+        mem_addr = instr[10:]
+        return decimal(mem_addr)
+      
+
 def hlt():
     return 0
 
@@ -121,8 +141,11 @@ instructions = {"00010" : movImm
                 , "01001" : ls
                 , "01101" : n_ot  
                 , "01110" : c_mp
+                , "01111" : jmp
+                , "11100" : jlt
+                , "11101" : jgt
+                , "11111" : je
                 , "11010" : hlt()}
-
 
 while ( command_list[i][:5]!='11010'):   #termination step is when we reach opcode of halt
     i=instructions[command_list[i][:5]](command_list[i])
