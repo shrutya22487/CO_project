112276faf59413398fe0e1b2ea2da73085cda273
Vedika Agarwal <131861772+27vedika@users.noreply.github.com>
2023-06-01 16:55:02 +0530

    Update sim_main.py

--- sim_main.py
+++ sim_main.py
@@ -1,21 +1,24 @@
-i , R0 , R1 , R2 , R3 , R4 , R5 , R6 , FLAGS =0, 0 , 0 , 0 , 0 , 0 , 0 , 0 , '0000000000000000'
+i , R0 , R1 , R2 , R3 , R4 , R5 , R6 , FLAGS =0, 0 , 0 , 0 , 0 , 0 , 0 , 0 , [0 for k in range (16)]
 reg = { "000" : R0 , "001" : R1 , "010" : R2 , "011" : R3 , "100" : R4 , "101" : R5 , "110" : R6 , "111" : FLAGS }  
 command_list=["0001000100000100", "0011100000001110", "1101000000000000"]
 l = len ( command_list )
 variables = [] 
 def registers():
+    global FLAGS
     s=''
     for j in reg:
         if j!="111":
             s+=binary(reg[j],16) + '\n' #gives the output of the registers as binary 16 bits
-    s+=FLAGS
+    FLAGS=list(map(str, FLAGS))
+    s+="".join(FLAGS)
     return s
 def binary(x,n): #x->Decimal number, n->No of bits
     lst=["0" for i in range (n)]
     for i in range (n-1, -1, -1):
         lst[i]=str(x%2)
         x=x//2
-    return "".join(lst)
+        y = "".join(lst)
+    return y
 def decimal(n): # converts from binary to decimal
     num, value , base = int( n ) , 0 , 1 
     tmp = num
@@ -25,21 +28,89 @@ def decimal(n): # converts from binary to decimal
         value = value + l * base
         base *= 2
     return value
+def add(instr):
+    global FLAGS
+    reg2= reg[instr[10:13]]
+    reg3= reg[instr[13:]]
+    reg1=instr[7:10]
+    reg[reg1]= reg2 + reg3
+    if reg[reg1] > (2**16 -1) or reg[reg1] < -(2**16):
+        FLAGS[12]=1
+        reg[reg1] = binary(0,16)
+    else:
+        FLAGS=[0 for k in range (16)]
+    
+    return i+1
+
+def sub(instr):
+    global FLAGS
+    reg2=reg[instr[10:13]]
+    reg3=reg[instr[13:]]
+    reg1=instr[7:10]
+    reg[reg1] = reg2 - reg3
+    if reg[reg2] < reg[reg3]:
+        FLAGS[12]=1
+        reg[reg1] = binary(0,16)
+    else:
+        FLAGS=[0 for k in range (16)]
+    
+    return i+1
+
+def mul(instr):
+    global FLAGS
+    reg2= reg[instr[10:13]]
+    reg3= reg[instr[13:]]
+    reg1=instr[7:10]
+    reg[reg1]= reg2 * reg3
+    if reg[reg1] > (2**16 -1) or reg[reg1] < -(2**16):
+        FLAGS[12]=1
+        reg[reg1] = binary(0,16)
+    else:
+        FLAGS=[0 for k in range (16)]
+    
+    return i+1
+
+def bit_xor(instr):
+    global FLAGS
+    reg2=reg[instr[10:13]]
+    reg3=reg[instr[13:]]
+    reg1=instr[7:10]
+    reg[reg1] = reg2 ^ reg3
+    FLAGS=[0 for k in range (16)]
+    return i+1
+
+def bit_or(instr):
+    global FLAGS
+    reg2=reg[instr[10:13]]
+    reg3=reg[instr[13:]]
+    reg1=instr[7:10]
+    reg[reg1] = reg2 | reg3
+    FLAGS=[0 for k in range (16)]
+    return i+1
+
+def bit_and(instr):
+    global FLAGS
+    reg2=reg[instr[10:13]]
+    reg3=reg[instr[13:]]
+    reg1=instr[7:10]
+    reg[reg1] = reg2 & reg3
+    FLAGS=[0 for k in range (16)]
+    return i+1
 
 def movImm(instr):
+    global FLAGS
     reg1=instr[6:9]
     val=decimal(instr[9:])
     reg[reg1]=val
-    global FLAGS
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def mov_reg(instr):
+    global FLAGS
     reg1=instr[10:13]
     reg2=reg[instr[13:]]
     reg[reg1]=reg2
-    global FLAGS
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def div(instr):
@@ -47,37 +118,37 @@ def div(instr):
     reg3=reg[instr[10:13]]
     reg4=reg[instr[13:]]
     if reg4==0:
-        FLAGS="0000000000001000"
+        FLAGS[12]=1
         reg["000"]=0
         reg["001"]=0
         return i+1
     reg["000"]=reg3//reg4
     reg["001"]=reg3%reg4
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def rs(instr):
+    global FLAGS
     reg1=instr[6:9]
     val=decimal(instr[9:])
     reg[reg1]=reg[reg1]>>val
-    global FLAGS
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def ls(instr):
+    global FLAGS
     reg1=instr[6:9]
     val=decimal(instr[9:])
     reg[reg1]=reg[reg1]<<val
-    global FLAGS
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def n_ot(instr):
+    global FLAGS
     reg1=reg[instr[10:13]]
     reg2=decimal(instr[13:])
     reg[reg1]=~reg2
-    global FLAGS
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def c_mp(instr):
@@ -85,57 +156,64 @@ def c_mp(instr):
     reg1=reg[instr[10:13]]
     reg2=reg[instr[13:]]
     if reg1<reg2:
-        FLAGS="0000000000000100"
+        FLAGS[13]=1
     elif reg1>reg2:
-        FLAGS="0000000000000010"
+        FLAGS[14]=1
     else:
-        FLAGS="0000000000000001"
+        FLAGS[15]=1
     return i+1
 
 def ld(instr):
+    global FLAGS
     reg1=instr[6:9]
     mem = int( instr[9:] )
     loc = decimal( mem ) - l - 1  
     try:
-        variables[ loc ]
+        variables[loc]
     except:
         variables.append( 0 )
-    # reg[reg1]=mem
-    global FLAGS
-    FLAGS="0000000000000000"
+    reg[reg1]=variables[loc] #Review this
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def st(instr):
+    global FLAGS
     mem = int( instr[9:] )
     reg1=reg[instr[6:9]]
     loc = decimal( mem ) - l - 1 
     variables[ loc ] = reg1
-    global FLAGS
-    FLAGS="0000000000000000"
+    FLAGS=[0 for k in range (16)]
     return i+1
 
 def jmp(instr):   # "01111"
+    global FLAGS
     mem_addr = instr[10:]
+    FLAGS=[0 for k in range (16)]
     return decimal(mem_addr)
   
 def jlt(instr):  # "11100"
-    if FLAGS[14] == "1":
+    global FLAGS
+    if FLAGS[13] == 1:
         mem_addr = instr[10:]
         return decimal(mem_addr)
+    FLAGS=[0 for k in range (16)]
     return i+1
       
 def jgt(instr):  # "11101"
-    if FLAGS[15] == "1":
+    global FLAGS
+    if FLAGS[14] == 1:
         mem_addr = instr[10:]
         return decimal(mem_addr)
+    FLAGS=[0 for k in range (16)]
     return i+1
   
 def je(instr):  # "11111"
-    if FLAGS[16] == "1":
+    global FLAGS
+    if FLAGS[15] == 1:
         mem_addr = instr[10:]
         return decimal(mem_addr)
+    FLAGS=[0 for k in range (16)]
     return i+1
-      
 
 def hlt():
     return 0
