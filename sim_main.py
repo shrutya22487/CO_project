6de37d5d02b7d90e3c9108099eb039c00f7c54c7
shrutya22487 <131897020+shrutya22487@users.noreply.github.com>
2023-06-03 13:02:37 +0530

    Update sim_main.py

--- sim_main.py
+++ sim_main.py
@@ -1,25 +1,37 @@
 i , R0 , R1 , R2 , R3 , R4 , R5 , R6 , FLAGS =0, 0 , 0 , 0 , 0 , 0 , 0 , 0 , [0 for k in range (16)]
-reg = { "000" : R0 , "001" : R1 , "010" : R2 , "011" : R3 , "100" : R4 , "101" : R5 , "110" : R6 , "111" : FLAGS }  
-l = ['D:\\test1.txt', 'D:\\test2.txt', 'D:\\test3.txt', 'D:\\test4.txt', 
-     'D:\\test5.txt' ]
+reg = {
+    "000": globals()["R0"],
+    "001": globals()["R1"],
+    "010": globals()["R2"],
+    "011": globals()["R3"],
+    "100": globals()["R4"],
+    "101": globals()["R5"],
+    "110": globals()["R6"],
+    "111": globals()["FLAGS"]
+}  
+l = [ 'D:\\test2.txt' , 'D:\\test3.txt', 'D:\\test4.txt', 
+    'D:\\test5.txt' ]
 #f =open("D:\\test1.txt")
 
 for i in l:
-    f = open( i )
+    f = open(i)
     command_list = f.readlines()
-    l = len ( command_list )
-    for i in range( l ):
-        command_list[i] = command_list[i][:16]
-    print( command_list )
+    l = len(command_list)
+    for i in range(l):
+        command_list[i] = command_list[i].strip()  # Remove trailing newline character
+
+
     variables = [] 
     def registers():
         global FLAGS
         s=''
         for j in reg:
             if j!="111":
-                s+=binary(reg[j],16) + '\n' #gives the output of the registers as binary 16 bits
+                s+=binary(reg[j],16) + ' ' #gives the output of the registers as binary 16 bits
         FLAGS=list(map(str, FLAGS))
-        s+="".join(FLAGS)
+        a = "".join( FLAGS )
+        #print( FLAGS )
+        s = s + ' ' + a
         return s
     def binary(x,n): #x->Decimal number, n->No of bits
         lst=["0" for i in range (n)]
@@ -39,10 +51,10 @@ for i in l:
         return value
     def add(instr):
         global FLAGS
-        print( instr[13:] )
+        #print( instr[10:13] )
         reg2= reg[instr[10:13] ]
-        reg3= reg[instr[13:] ]
-        reg1= instr[7:10]
+        reg3= reg[instr[13:16] ]
+        reg1=instr[7:10]
         reg[reg1]= reg2 + reg3
         if reg[reg1] > (2**16 -1) or reg[reg1] < -(2**16):
             FLAGS[12]=1
@@ -54,18 +66,17 @@ for i in l:
 
     def sub(instr):
         global FLAGS
-        reg2=instr[10:13]
-        reg3=instr[13:]
-        reg1=instr[7:10]
-        print( "reg1", reg1 , "reg2" , reg2 , "reg3 " , reg3)
-        reg[reg1] = reg[ reg2 ] - reg[ reg3 ]
-        if reg[reg2] < reg[reg3]:
-            FLAGS[12]=1
-            reg[reg1] = binary(0,16)
+        reg2 = reg[instr[10:13]]
+        reg3 = reg[instr[13:]]
+        reg1 = instr[7:10]
+        reg[reg1] = reg2 - reg3
+        if reg2 < reg3:
+            FLAGS[12] = 1
+            reg[reg1] = binary(0, 16)
         else:
-            FLAGS=[0 for k in range (16)]
-        
-        return i+1
+            FLAGS = [0 for k in range(16)]
+
+        return i + 1
 
     def mul(instr):
         global FLAGS
@@ -174,18 +185,19 @@ for i in l:
             FLAGS[15]=1
         return i+1
 
+    variables = [0] * (2 ** 16)
+
     def ld(instr):
         global FLAGS
         reg1=instr[6:9]
         mem = int( instr[9:] )
-        loc = decimal( mem ) - l + 1
-        print( variables )  
+        loc = decimal( mem ) - l + 1  
         try:
             variables[loc]
         except:
             variables.append( 0 )
-        reg[reg1] = variables[loc] #Review this
-        FLAGS = [0 for k in range (16)]
+        reg[reg1]=variables[loc] #Review this
+        FLAGS=[0 for k in range (16)]
         return i+1
 
     def st(instr):
@@ -202,7 +214,7 @@ for i in l:
         mem_addr = instr[10:]
         FLAGS=[0 for k in range (16)]
         return decimal(mem_addr)
-    
+
     def jlt(instr):  # "11100"
         global FLAGS
         if FLAGS[13] == 1:
@@ -218,7 +230,7 @@ for i in l:
             return decimal(mem_addr)
         FLAGS=[0 for k in range (16)]
         return decimal(mem_addr)
-    
+
     def je(instr):  # "11111"
         global FLAGS
         if FLAGS[15] == 1:
@@ -230,36 +242,42 @@ for i in l:
     def hlt():
         return 0
     def memory_dump():
-        for i in command_list:
-            print( '000000000' + i )
-        for i in range( l , 129 ):
-            print( '0000000000000000' )
+        for i, cmd in enumerate(command_list):
+            print( cmd.strip() )
+        for i in range(l, 129):
+            print('0000000000000000')
 
-    instructions = { "00000" : add(command_list[i])
-                    , "00001" : sub(command_list[i])
-                    , "00010" : movImm(command_list[i])
-                    , "00011" : mov_reg(command_list[i])
-                    , "00100" : ld(command_list[i]) 
-                    , "00101" : st(command_list[i])
-                    , "00110" : mul(command_list[i])
-                    , "00111" : div(command_list[i])
-                    , "01000" : rs(command_list[i]) 
-                    , "01001" : ls(command_list[i])
-                    , "01010" : bit_xor(command_list[i])
-                    , "01011" : bit_or(command_list[i])
-                    , "01100" : bit_and(command_list[i])
-                    , "01101" : n_ot(command_list[i])  
-                    , "01110" : c_mp(command_list[i])
-                    , "01111" : jmp(command_list[i])
-                    , "11100" : jlt(command_list[i])
-                    , "11101" : jgt(command_list[i])
-                    , "11111" : je(command_list[i])
-                    , "11010" : hlt()}
+
+    instructions = {
+        "00000": add,
+        "00001": sub,
+        "00010": movImm,
+        "00011": mov_reg,
+        "00100": ld,
+        "00101": st,
+        "00110": mul,
+        "00111": div,
+        "01000": rs,
+        "01001": ls,
+        "01010": bit_xor,
+        "01011": bit_or,
+        "01100": bit_and,
+        "01101": n_ot,
+        "01110": c_mp,
+        "01111": jmp,
+        "11100": jlt,
+        "11101": jgt,
+        "11111": je,
+        "11010": hlt
+    }
     i = 0
+    
     while ( command_list[i][:5]!='11010'):   #termination step is when we reach opcode of halt
-        i=instructions[command_list[i][:5]]
+        i = instructions[command_list[i][:5]](command_list[i])
         s=registers()
-        print(binary(i,7)+" "+s+" ")
+        print(binary(i - 1 , 7 )+" "+s+" ")
+    s=registers()
+    print(binary(i,7)+" "+s+" ")
     memory_dump()
     print("\n")
     f.close()
