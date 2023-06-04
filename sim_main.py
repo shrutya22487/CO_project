3cb97adc3590f726000be4ca1d9574a48a599777
Vedika Agarwal <131861772+27vedika@users.noreply.github.com>
2023-06-04 13:06:35 +0530

    Update sim_main.py

--- sim_main.py
+++ sim_main.py
@@ -1,4 +1,4 @@
-i , R0 , R1 , R2 , R3 , R4 , R5 , R6 , FLAGS =0, 0 , 0 , 0 , 0 , 0 , 0 , 0 , [0 for k in range (16)]
+i , R0 , R1 , R2 , R3 , R4 , R5 , R6 , FLAGS =0, 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0
 reg = {
     "000": globals()["R0"],
     "001": globals()["R1"],
@@ -14,7 +14,7 @@ l = ['D:\\test1.txt', 'D:\\test2.txt', 'D:\\test3.txt', 'D:\\test4.txt',
 #f =open("D:\\test1.txt")
 
 for file in l:
-    f = open(file)
+    f = open("input.txt")
     command_list = f.readlines()
     l = len(command_list)
     for i in range(l):
@@ -22,21 +22,24 @@ for file in l:
 
 
     variables = [] 
+    def set_flags(FLAGS, index):
+        FLAGS=list(binary(FLAGS, 16))
+        FLAGS[index]=1
+        FLAGS=list(map(str, FLAGS))
+        FLAGS=decimal("".join(FLAGS))
+        return FLAGS
+
     def registers():
-        global FLAGS
         s=''
         for j in reg:
-            if j!="111":
-                s+=binary(reg[j],16) + ' ' #gives the output of the registers as binary 16 bits
-        FLAGS=list(map(str, FLAGS))
-        s+="".join(FLAGS)
+            s+=binary(reg[j],16) + ' ' #gives the output of the registers as binary 16 bits
         return s
     def binary(x,n): #x->Decimal number, n->No of bits
         lst=["0" for i in range (n)]
         for i in range (n-1, -1, -1):
             lst[i]=str(x%2)
             x=x//2
-            y = "".join(lst)
+        y = "".join(lst)
         return y
     def decimal(n): # converts from binary to decimal
         num, value , base = int( n ) , 0 , 1 
@@ -48,145 +51,131 @@ for file in l:
             base *= 2
         return value
     def add(instr):
-        global FLAGS
         print( instr[10:13] )
         reg2= reg[instr[10:13] ]
         reg3= reg[instr[13:16] ]
         reg1=instr[7:10]
         reg[reg1]= reg2 + reg3
         if reg[reg1] > (2**16 -1) or reg[reg1] < -(2**16):
-            FLAGS[12]=1
+            reg["111"]=set_flags(reg["111"], 12)
             reg[reg1] = binary(0,16)
         else:
-            FLAGS=[0 for k in range (16)]
+            reg["111"]=0
         
         return i+1
 
     def sub(instr):
-        global FLAGS
         reg2 = reg[instr[10:13]]
         reg3 = reg[instr[13:]]
         reg1 = instr[7:10]
         reg[reg1] = reg2 - reg3
         if reg2 < reg3:
-            FLAGS[12] = 1
+            reg["111"]=set_flags(reg["111"], 12)
             reg[reg1] = binary(0, 16)
         else:
-            FLAGS = [0 for k in range(16)]
+            reg["111"]=0
 
         return i + 1
 
     def mul(instr):
-        global FLAGS
         reg2= reg[instr[10:13]]
         reg3= reg[instr[13:]]
         reg1=instr[7:10]
         reg[reg1]= reg2 * reg3
         if reg[reg1] > (2**16 -1) or reg[reg1] < -(2**16):
-            FLAGS[12]=1
+            reg["111"]=set_flags(reg["111"], 12)
             reg[reg1] = binary(0,16)
         else:
-            FLAGS=[0 for k in range (16)]
+            reg["111"]=0
         
         return i+1
 
     def bit_xor(instr):
-        global FLAGS
         reg2=reg[instr[10:13]]
         reg3=reg[instr[13:]]
         reg1=instr[7:10]
         reg[reg1] = reg2 ^ reg3
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def bit_or(instr):
-        global FLAGS
         reg2=reg[instr[10:13]]
         reg3=reg[instr[13:]]
         reg1=instr[7:10]
         reg[reg1] = reg2 | reg3
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def bit_and(instr):
-        global FLAGS
         reg2=reg[instr[10:13]]
         reg3=reg[instr[13:]]
         reg1=instr[7:10]
         reg[reg1] = reg2 & reg3
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def movImm(instr):
-        global FLAGS
         reg1=instr[6:9]
         val=decimal(instr[9:])
         reg[reg1]=val
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def mov_reg(instr):
-        global FLAGS
         reg1=instr[10:13]
         reg2=reg[instr[13:]]
         reg[reg1]=reg2
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def div(instr):
-        global FLAGS
         reg3=reg[instr[10:13]]
         reg4=reg[instr[13:]]
         if reg4==0:
-            FLAGS[12]=1
+            reg["111"]=set_flags(reg["111"], 12)
             reg["000"]=0
             reg["001"]=0
             return i+1
         reg["000"]=reg3//reg4
         reg["001"]=reg3%reg4
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def rs(instr):
-        global FLAGS
         reg1=instr[6:9]
         val=decimal(instr[9:])
         reg[reg1]=reg[reg1]>>val
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def ls(instr):
-        global FLAGS
         reg1=instr[6:9]
         val=decimal(instr[9:])
         reg[reg1]=reg[reg1]<<val
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def n_ot(instr):
-        global FLAGS
         reg1=reg[instr[10:13]]
         reg2=decimal(instr[13:])
         reg[reg1]=~reg2
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def c_mp(instr):
-        global FLAGS
         reg1=reg[instr[10:13]]
         reg2=reg[instr[13:]]
         if reg1<reg2:
-            FLAGS[13]=1
+            reg["111"]=set_flags(reg["111"], 13)
         elif reg1>reg2:
-            FLAGS[14]=1
+            reg["111"]=set_flags(reg["111"], 14)
         else:
-            FLAGS[15]=1
+            reg["111"]=set_flags(reg["111"], 15)
         return i+1
 
     variables = [0] * (2 ** 16)
 
     def ld(instr):
-        global FLAGS
         reg1=instr[6:9]
         mem = int( instr[9:] )
         loc = decimal( mem ) - l + 1  
@@ -194,47 +183,42 @@ for file in l:
             variables[loc]
         except:
             variables.append( 0 )
-        reg[reg1]=variables[loc] #Review this
-        FLAGS=[0 for k in range (16)]
+        reg[reg1]=variables[loc] #Review
+        reg["111"]=0
         return i+1
 
     def st(instr):
-        global FLAGS
         mem = int( instr[9:] )
         reg1 = reg[ instr[6:9] ]
         loc = decimal( mem ) - l + 1 
         variables[ loc ] = reg1
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def jmp(instr):   # "01111"
-        global FLAGS
         mem_addr = instr[10:]
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return decimal(mem_addr)
 
     def jlt(instr):  # "11100"
-        global FLAGS
-        if FLAGS[13] == 1:
+        if binary(reg["111"],16)[13] == "1":
             mem_addr = instr[10:]
             return decimal(mem_addr)
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
         
     def jgt(instr):  # "11101"
-        global FLAGS
-        if FLAGS[14] == 1:
+        if binary(reg["111"],16)[14] == "1":
             mem_addr = instr[10:]
             return decimal(mem_addr)
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def je(instr):  # "11111"
-        global FLAGS
-        if FLAGS[15] == 1:
+        if binary(reg["111"],16)[15] == "1":
             mem_addr = instr[10:]
             return decimal(mem_addr)
-        FLAGS=[0 for k in range (16)]
+        reg["111"]=0
         return i+1
 
     def hlt():
